931a24b8e8982ab17eccbbf0df87f422e4417b86
lucasalmeida <lac.lucas96@gmail.com>
2023-03-06 19:30:01 -0300

    feat: cria rota acomodacoes

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const routes = require('./routes');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-	res.send({ message: 'hello, world!' });
-});
+app.use('/api', routes);
 
 app.listen(process.env.PORT, () => {
 	console.log(`Servidor rodando em: http://localhost:${process.env.PORT}`);
