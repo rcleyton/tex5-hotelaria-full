d9ea3a015313b29f53d62fd17013e34eec715936
Kaue Cardinalli <kauecardinalli@hotmail.com>
2023-03-30 15:59:43 -0300

    feature: CSS das tabelas admin

--- web/src/views/AdminServicosAdicionaisView.vue
+++ web/src/views/AdminServicosAdicionaisView.vue
@@ -1,92 +1,116 @@
 <template>
-<section>
-	<div>
-		<form style="display:flex" enctype="multipart/form-data" @submit="sendData()" >
-			<div style="margin-right:1rem">
-				<label for="item"></label>
-				<input type="text"
-				name="item"
-				id="item"
-				placeholder="Item"
-				v-model="form.item">
-			</div>
-			<div style="margin-right:1rem">
-				<label for="descricao"></label>
-				<input type="text"
-				name="descricao"
-				id="descricao"
-				placeholder="Descricao do produto"
-				v-model="form.descricao">
-			</div>
-			<div style="margin-right:1rem">
-				<label for="valor"></label>
-				<input type="number"
-				name="valor"
-				id="valor"
-				placeholder="Valor do produto"
-				v-model="form.valor">
-			</div>
-			<div style="margin-right:1rem">
-				<label for="local"></label>
-				<input type="text"
-				name="local"
-				id="local"
-				placeholder="Local de venda"
-				v-model="form.local">
-			</div>	
-			<button type="submit">Cadastrar</button>	
-		</form>
+	<div class="main_content">
+		<div class="fixed-form">
+			<section class="form_main">
+				<div class="form_default">
+					<legend class="section_title">Cadastrar Serviço Adicional</legend>
+					<form
+						class="form_geral"
+						style="display: flex"
+						enctype="multipart/form-data"
+						@submit="sendData()"
+					>
+						<div class="form_field">
+							<label for="item"></label>
+							<input
+								class="input_form"
+								type="text"
+								name="item"
+								id="item"
+								placeholder="Item"
+								v-model="form.item"
+							/>
+						</div>
+						<div class="form_field">
+							<label for="descricao"></label>
+							<input
+								class="input_form"
+								type="text"
+								name="descricao"
+								id="descricao"
+								placeholder="Descricao do produto"
+								v-model="form.descricao"
+							/>
+						</div>
+						<div class="form_field">
+							<label for="valor"></label>
+							<input
+								class="input_form"
+								type="number"
+								name="valor"
+								id="valor"
+								placeholder="Valor do produto"
+								v-model="form.valor"
+							/>
+						</div>
+						<div class="form_field">
+							<label for="local"></label>
+							<input
+								class="input_form"
+								type="text"
+								name="local"
+								id="local"
+								placeholder="Local de venda"
+								v-model="form.local"
+							/>
+						</div>
+						<button class="button_form" type="submit">Cadastrar</button>
+					</form>
+				</div>
+			</section>
+		</div>
+		<div>
+			<table>
+				<thead>
+					<tr>
+						<th>Id</th>
+						<th>Item</th>
+						<th>Descrição</th>
+						<th>Valor</th>
+						<th>Local</th>
+						<th>Editar</th>
+						<th>Excluir</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr v-for="item in servicosArr" :key="item.id_servico_adicional">
+						<td>{{ item.id_servico_adicional }}</td>
+						<td>{{ item.item }}</td>
+						<td>{{ item.descricao }}</td>
+						<td>{{ item.valor }}</td>
+						<td>{{ item.local }}</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
 	</div>
-	<div>
-		<table>
-			<thead>
-				<tr>
-					<th>Id</th>
-					<th>Item</th>
-					<th>Descrição</th>
-					<th>Valor</th>
-					<th>Local</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr v-for="item in servicosArr" :key="item.id_servico_adicional">
-					<td style="text-align:center" > {{ item.id_servico_adicional }} </td>
-					<td style="text-align:center" > {{ item.item }} </td>
-					<td style="text-align:center" > {{ item.descricao }} </td>
-					<td style="text-align:center" > {{ item.valor }} </td>
-					<td style="text-align:center" > {{ item.local }} </td>
-				</tr>
-			</tbody>
-		</table>
-	</div>
-</section>
-    
 </template>
 
 <script>
-import axios from 'axios'
+import axios from 'axios';
 export default {
 	name: 'AdmimServicosAdicionais',
 	data() {
 		return {
-			servicosArr: "",
+			servicosArr: '',
 			form: {
-				id_servico_adicional : "",
-				item : "",
-				descricao : "",
-				valor : "",
-				local : ""
-			}
-		}
+				id_servico_adicional: '',
+				item: '',
+				descricao: '',
+				valor: '',
+				local: '',
+			},
+		};
 	},
 
 	methods: {
 		async getData() {
-			await axios.get("http://localhost:3000/api/servicosAdicionais")
+			await axios
+				.get('http://localhost:3000/api/servicosAdicionais')
 				.then((resolve) => {
-					this.servicosArr = resolve.data
+					this.servicosArr = resolve.data;
 				})
-				.catch(error => console.log(error))
+				.catch((error) => console.log(error));
 		},
 
 		async sendData() {
@@ -94,27 +118,23 @@ export default {
 				item: this.form.item,
 				descricao: this.form.descricao,
 				valor: this.form.valor,
-				local: this.form.local
-			}
+				local: this.form.local,
+			};
 
-			await axios.post("http://localhost:3000/api/servicosAdicionais",data)
+			await axios
+				.post('http://localhost:3000/api/servicosAdicionais', data)
 				.then((res) => res.data)
-				.catch((error) => console.log(error))
-			
-		}
+				.catch((error) => console.log(error));
+		},
 	},
 
 	mounted() {
-		this.getData()
-		
-	}
-}
-
-
-
-
+		this.getData();
+	},
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+@use 'src/sass/sass-utils/_colors.scss' as v;
+@import '../sass/components/tables.scss';
 </style>
